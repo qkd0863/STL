2e213e9e03ba3f5da91677d62022215d82e4d881
gunheelee-c106-20 <qsd0812@me.com>
2024-03-25 19:05:14 +0900

    3/25

--- Project8/FileName.cpp
+++ Project8/FileName.cpp
@@ -1,30 +1,83 @@
 #include "save.h"
 #include <string>
 #include <array>
+#include <numeric>
 //-----------------------------------------------------------------
-// 2024.03.19 1학기 stl 화 5~6 목 5~6                       (3주 2)
+// 2024.03.25 1학기 stl 화 5~6 목 5~6                       (4주 1)
 // 
-// 실행 파일의 메모리 영역 - stack code data free store(heap)
+// 메모리 동적할당 - RAII
+// 
+// 앞으로 c++ 에서 사용하지 않아야 할 것들
+// char*  -> string 
+// T[N]  -> array<T,N>
+// T*    -> unique_ptr, shared_ptr( 스마트 포인터로 대체)
 //-----------------------------------------------------------------
 
 
-// 문제 프로그램에서 이용할 수 있는 전역변수의 최대 크기는?
+// 문제 사용자가 입력한 int num 만큼 메모리를 동적할당하라
+// int를 num개 동적할당한다
+// 이 메모리를 1부터 시작하는 정수로 채운다
+// 이 메모리의 합계를 화면에 출력한다
+// 이 모든 과정을 영원히 반복하라
 
-int num[100000000]{};	//초기화 하지 않은 data == default로 초기화한 data
-//int num[100000000]{1};  //num[0]만 초기화 할 수 없다, 400mb이미지를 복사해야한다
+// -> run-time에서야 num값이 결정된다 --> new T[num]
+// -> complie time에 모든 값이 결정 --> constexpr
 
-int main(){
-	num[99999999] = 1234567890;
-	cout << num[99999999] << endl;
-	cout << "키를 누르면 프로그램 끝 - ";
-	char c;
-	cin >> c;
+class Dog
+{
+public:
+	Dog() { cout << "생성" << endl; }
+	~Dog() { cout << "소멸" << endl; }
+};
 
+
+// RAII idiom(관용구) - 메모리, 파일, jthread
+
+class Smart_ptr
+{
+	Dog* p;
+public:
+	Smart_ptr(Dog* p):p{p}{}
+	~Smart_ptr()
+	{
+		delete p;
+	}
+};
+
+void f() 
+{
+	cout << "f 시작" << endl;
 	
+	{//예외 발생
+		Smart_ptr p{ new Dog };	// stack-unwinding   c++ 언어가 보장
+		throw 1234;		// catch 라인으로 이동
+	}
 
+
+	cout << "f 종료" << endl;
+};
+
+int main()
+{
 	save("FileName.cpp");
+	cout << "메인 시작" << endl;
+
+	try
+	{
+		f();// 자원을 확보하고 정리하는 함수
+	}
+	catch (...)
+	{
+
+	}
+
+	cout << "메인 끝" << endl;
 }
-	
 
 
-// 실제로 실행해보면 메모리 위치가 다르다	
+
+
+// 메모리 해제를 까먹음
+// 메모리를 여러번 해제함 - dangling 프로그램이 바로 죽는다
+// 지역을 벗어나면 반드시 소멸한다 - 연결시켜줘야한다
+// 예외 처리가되면 소멸이 안될 수 있다
