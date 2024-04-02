87ce68dc5c0fd32f9997709cef14dbf9277b5caa
qkd <qkd7183@gmail.com>
2024-04-02 17:20:49 +0900

    4/2

--- Project8/FileName.cpp
+++ Project8/FileName.cpp
@@ -1,4 +1,3 @@
-#include "save.h"
 #include <string>
 #include <array>
 #include <numeric>
@@ -7,54 +6,37 @@
 #include <random>
 #include <print>
 #include <functional>
+#include "String.h"
+#include "save.h"
 
 //-----------------------------------------------------------------
-// 2024.04.1 1학기 stl				                        (5주 1)
+// 2024.04.2 1학기 stl				                        (5주 2)
 // 
-// 
-// callable type - 무한개의 타입이 있다
-// 어떤 함수에 다른 함수를 전달한다
-// sort할 때 어떤 기준으로 정렬할지를 어떻게 sort함수에게 알려주나?
-// 1. 함수 포인터를 전달한다
-// 2. 람다를 전달한다 - visual studio는 함수 객체로 구현
-// 3. ()연산자를 오버로딩한 클래스의 객체를 전달한다 (funtion call - () )
-//  함수 객체(function object)
-// 1과 2,3의 다른점 - 2,3은 상태를 가질 수 있다
-// 
-// 호출가능한 타입을 대표하는 타입을 만들었다 - function 
 // 
 //-----------------------------------------------------------------
 
-
-class String
-{
-	size_t len{};
-	unique_ptr<char[]> p;
-public:
-	String(const char* s):len(strlen(s))
-	{
-		p = make_unique<char[]>(len);
-		memcpy(p.get(), s, len);
-	}
-
-	friend ostream& operator<<(ostream& os, const String& str)
-	{
-		for (size_t i = 0; i < str.len; ++i)
-			os << str.p.get()[i];
-		return os;
-	}
-};
-
+extern bool 관찰;
 
 
 int main()
 {
-	save("FileName.cpp");
-	String s{ "STL을 관찰하기 위한 클래스입니다" };
-	//String t = s;
-	cout << s << endl;
-	//cout << t << endl;
+	//save("FileName.cpp");
+	
+	array<String, 5> a{ "dsadsadasdad","sadasfsafasd","wqewqreqe","wqewqrqwwe","xzczxcvzxc" };
+	
+	
+	// sort를 사용하여 a가 관리하는 String을 오름차순으로 정렬한 후 출력하라
+
+	관찰 = true;
+	for (const String& s : a)
+		sort(s.getMem(), s.getMem() + s.getLen());
+	관찰 = false;
+
+	for (String& s : a)
+		cout << s << endl;
+
 }
 
 
 
+//String t = s;		//디폴트 복사생성자를 만들 수 없다 - 스마트포인터는 복사생성이 불가
---
+++ Project8/String.h
@@ -0,0 +1,48 @@
+//-----------------------------------------------------------------------------------------------------------------------------------------
+// String.h		STL 동작을 관찰하기 위한 클래스
+// 
+// 2024 4/2 시작
+//-----------------------------------------------------------------------------------------------------------------------------------------
+
+#pragma once
+#include <memory>
+#include <iostream>
+
+class String
+{
+	size_t len{};
+	std::unique_ptr<char[]> p;
+	size_t id{};
+public:
+	// 디폴트 생성자 - 2024 4 2
+	String();
+
+	String(const char* s);
+	~String();	//소멸자는 관찰하려고 코딩하였다 2024 4 2
+
+	// 복사생성과 복사할당 - 2024 4 2
+	String(const String&);
+	String& operator=(const String&);
+
+
+	// 이동생성과 이동할당 2024 4 2
+	String(String&&);
+	String& operator=(String&&);
+
+
+
+	//sort에서 필요한 getLen 2024 4 2
+	size_t getLen()const; 
+	//p가 가리키는 메모리를 알려준다 2024 4 2
+	char* getMem() const;
+
+	friend std::ostream& operator<<(std::ostream& os, const String& str)
+	{
+		for (size_t i = 0; i < str.len; ++i)
+			os << str.p.get()[i];
+		return os;
+	}
+
+private:
+	static size_t uid;
+};
