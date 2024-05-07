5530b98ec987cc488615d20a57a2e5cac9ad0e47
asd <asdf@asd>
2024-05-07 17:22:13 +0900

    5/7

--- Project8/FileName.cpp
+++ Project8/FileName.cpp
@@ -1,59 +1,36 @@
-#include <forward_list>
-#include <list>
-#include <deque>
-#include <vector>
-#include <iterator>
-#include "save.h"
 #include "String.h"
+#include "save.h"
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <span>
+
+
 //-----------------------------------------------------------------
-// 2024.04.30 1학기 stl				                        (10주 1)
+// 2024.04.30 1학기 stl				                        (10주 2)
 //  
-// iterator -iterator are a generalization of pointers
-// 포인터를 일반화 한 것이 반복자다. 이걸 사용하면 자료구조가 뭔지
-// 신경쓸 필요없이 동일한 방식으로 코딩할 수 있다
+//
 //-----------------------------------------------------------------
 
 extern bool 관찰;
 
-
 template <class 반복자>
 void f(반복자 iter)
 {
-	//c++17 이후
-	// 벡터만 컴파일 통과
-	// 다른 컨테이너는 실패 - SFINAE
+	cout << typeid(iterator_traits<반복자>::iterator_category).name() << endl;
+	//c++17
 	cout << typeid(반복자::iterator_concept).name() << endl;
-	//iterator_category
 }
+
+
 
 int main()	
 {
-	//save("FileName.cpp");
-	// 반복자란 무엇인가.
-	// 출력스트림 반복자(stream 반복자, 반복자 어댑터)의 동작을 통해 반복자가 
-	// 무엇인지 알아본다
-	// 반복자는 포인터가 절대 아니다
-
-	// 문제 반복자는 6가지 종류가 있다
-	// 함수 f에게 반복자를 인자로 넘겨주면 어떤 종류인지 출력하게 하라
+	save("FileName.cpp");
 	
-	// 문제 벡터가 contiguous 인지 판별하려면 질문을 바꿔야 한다
-
-
-
-	f(istream_iterator<String>{cin});
-	f(ostream_iterator<String>{cout});
-	f(forward_list<char>::iterator{});
-	f(list<double>::reverse_iterator{});
-	f(deque<int>{}.cbegin());
-	f(vector<int>{}.begin());	//벡터의 반복자는 다른 반복자 종류가 될 수 있다 
+	string s{ "1357924680" };
 	
-	f(int{});
-
-
-
-
-
-
 	
+	sort(s.rbegin(), s.rend());
+	cout << s << endl;
 }                
--- Project8/String.h
+++ Project8/String.h
@@ -5,11 +5,82 @@
 // 2024 4 15 noexecpt
 // 2024 4 16 operator==
 // 2024 4 30 operator< 
+// 2024 5 7  begin() end()
+// 2024 5 7  rbegin() rend()의 결과는 class 객체이어야 한다
 //-----------------------------------------------------------------------------------------------------------------------------------------
 
 #pragma once
 #include <memory>
 #include <iostream>
+
+//String이 리턴하는 반복자 - iterator
+class String_iterator
+{
+public:
+	using difference_type = ptrdiff_t;
+	using value_type = char;
+	using pointer = char*;
+	using reference = char&;
+	using iterator_category = std::random_access_iterator_tag;
+
+	using iterator_concept = std::contiguous_iterator_tag;
+
+private:
+	char* p;
+public:
+	String_iterator(char* p) :p{ p } {}
+
+
+	char* operator++()
+	{
+		return ++p;
+	}
+	char operator*()const
+	{
+		return *p;
+	}
+
+	bool operator==(const String_iterator& rhs)const
+	{
+		return p == rhs.p;
+	}
+
+};
+
+//역방향 반복자 - iterator adaptor
+class String_reverse_iterator
+{
+public:
+	using difference_type = int;
+	using value_type = char;
+	using pointer = char*;
+	using reference = char&;
+	using iterator_category = std::random_access_iterator_tag;
+
+	using iterator_concept = std::contiguous_iterator_tag;
+
+private:
+	char* p;
+public:
+	String_reverse_iterator(char* p) :p{ p } {}
+
+
+	char* operator++()
+	{
+		return --p;
+	}
+	char operator*()const 
+	{
+		return *(p - 1);
+	}
+
+	bool operator==(const String_reverse_iterator& rhs)const
+	{
+		return p == rhs.p;
+	}
+
+};
+
 
 class String
 {
@@ -40,6 +111,12 @@ public:
 	//p가 가리키는 메모리를 알려준다 2024 4 2
 	char* getMem() const;
 
+	// 2024 5 7
+	String_iterator begin()const { return String_iterator{ p.get() }; };
+	String_iterator end()const {return String_iterator{p.get()+len}; };
+	String_reverse_iterator rbegin()const { return String_reverse_iterator{ p.get() + len }; };
+	String_reverse_iterator rend()const { return String_reverse_iterator{ p.get() }; };
+	
 	friend std::ostream& operator<<(std::ostream& os, const String& str)
 	{
 		for (size_t i = 0; i < str.len; ++i)
@@ -57,6 +134,7 @@ public:
 	bool operator<(const String& rhs)const;
 
 
+	
 private:
 	static size_t uid;
 };
