0c255bd63e1e5b8d57929066815143ef464e1826
asd <asdf@asd>
2024-05-21 17:22:45 +0900

    5/21

--- Project8/FileName.cpp
+++ Project8/FileName.cpp
@@ -1,53 +1,41 @@
 #include <string>
-#include <vector>
-#include <set>
+#include <list>
+#include <map>
 #include <fstream>
+#include <algorithm>
 #include "String.h"
 #include "save.h"
 
 //-----------------------------------------------------------------
-// 2024.04.30 1학기 stl				                        (11주 2)
+// 2024.04.30 1학기 stl				                        (12주 2)
 //  
-// Associative Conatainer - set
-// set은 어떻게 정렬하는가
-// 디폴트 less<Key> - > operator<를 이용한다
+// Associative Conatainer - map<key, value> -> dictionary
+// map -> 연관배열처럼 사용할 수 있다 인덱스가 int가 아니다
 // 
 //-----------------------------------------------------------------
 
 extern bool 관찰;
 
 
-bool 찾았다면(multiset<String> ms)
-{
-
-}
 
 
 int main()	
 {
-	save("FileName.cpp");
-	//문제 "이상한 나라의 앨리스.txt" 파일에 있는 단어를 mutiset에 읽어왔다
-	
+	//save("FileName.cpp");
+	// 그룹과 멤버
+	map<String, list<String>> m{ {"아이브",{"안유진","가을","레이","장원영","리즈","이서"}} };
 
-	multiset<String> s; //(*)==함수
-	ifstream in{ "이상한 나라의 앨리스.txt" };
-	if (not in)
-		return 123456;
-	
-	
-	s = { istream_iterator<String>{in}, {} };
-	// 문제 찾는 단어가 set에 있는지 알려준다
-	while (true)
+	m.insert(pair<String, list<String>>{"블랙핑크", { "지수","제니","로제","리사" }});
+	m.insert(make_pair("아이유", list<String>{"아이유"}));
+	m["르세라핌"] = { "사쿠라","김채원","허윤진","카즈하","홍은채"};
+
+	//전체 출력
+
+	for (const auto& [그룹, 멤버] : m)
 	{
-		cout << "찾을 단어는?";
-		String word;
-		cin >> word;
-
-		if (s.find(word) != s.end())	//s.contains(word) 이 단어가 있는지 bool로 반환
-			cout << word << "는 앨리스에 있는 단어입니다" << endl;
-		else
-			cout << "그런 단어 없습니다" << endl;
+		cout << 그룹 << " - " << 멤버.size() << "명 ";
+		for (const auto& a : 멤버)
+			cout << a << " ";
+		cout << endl;
 	}
-	cout << s.size();
-	
 }                
