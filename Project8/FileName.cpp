c8fc5c861158621c1ca29d12e82f1a6626f999a7
gunheelee-c106-20 <qsd0812@me.com>
2024-05-27 19:04:32 +0900

    5/27

--- Project8/FileName.cpp
+++ Project8/FileName.cpp
@@ -1,41 +1,37 @@
-#include <string>
-#include <list>
 #include <map>
+#include <unordered_map>
+#include <unordered_set>
 #include <fstream>
-#include <algorithm>
+#include <random>
 #include "String.h"
 #include "save.h"
 
 //-----------------------------------------------------------------
-// 2024.04.30 1학기 stl				                        (12주 2)
+// 2024.04.30 1학기 stl				                        (13주 1)
 //  
-// Associative Conatainer - map<key, value> -> dictionary
-// map -> 연관배열처럼 사용할 수 있다 인덱스가 int가 아니다
-// 
+// unordered associative container
+// unordered의 의미
+// 메모리 구조 어떻게?
+// 내가 만든 String도 관리가능해?
 //-----------------------------------------------------------------
 
 extern bool 관찰;
 
-
-
+default_random_engine dre;
+uniform_int_distribution uid{1,99999};
+normal_distribution nd{ 0.0,0.1 };
 
 int main()	
 {
 	//save("FileName.cpp");
-	// 그룹과 멤버
-	map<String, list<String>> m{ {"아이브",{"안유진","가을","레이","장원영","리즈","이서"}} };
 
-	m.insert(pair<String, list<String>>{"블랙핑크", { "지수","제니","로제","리사" }});
-	m.insert(make_pair("아이유", list<String>{"아이유"}));
-	m["르세라핌"] = { "사쿠라","김채원","허윤진","카즈하","홍은채"};
 
-	//전체 출력
+	unordered_set<int> us{ 1,2,3,4 };
 
-	for (const auto& [그룹, 멤버] : m)
-	{
-		cout << 그룹 << " - " << 멤버.size() << "명 ";
-		for (const auto& a : 멤버)
-			cout << a << " ";
-		cout << endl;
-	}
+
+	us.insert(7);
+	us.insert(10);
+	for (int num : us)
+		cout << num << endl;
+
 }                
