84191336259c2b8b6852ee63935f2ffbd901bab7
gunheelee-c106-20 <qsd0812@me.com>
2024-05-28 17:24:14 +0900

    5/28

--- Project8/FileName.cpp
+++ Project8/FileName.cpp
@@ -1,37 +1,17 @@
-#include <map>
-#include <unordered_map>
-#include <unordered_set>
-#include <fstream>
-#include <random>
 #include "String.h"
 #include "save.h"
 
 //-----------------------------------------------------------------
-// 2024.04.30 1학기 stl				                        (13주 1)
+// 2024.04.30 1학기 stl				                        (13주 2)
 //  
-// unordered associative container
-// unordered의 의미
-// 메모리 구조 어떻게?
-// 내가 만든 String도 관리가능해?
+// 
 //-----------------------------------------------------------------
 
 extern bool 관찰;
 
-default_random_engine dre;
-uniform_int_distribution uid{1,99999};
-normal_distribution nd{ 0.0,0.1 };
 
 int main()	
 {
 	//save("FileName.cpp");
-
-
-	unordered_set<int> us{ 1,2,3,4 };
-
-
-	us.insert(7);
-	us.insert(10);
-	for (int num : us)
-		cout << num << endl;
-
+	
 }                
