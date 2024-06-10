52117c8661730b5b404eeb5f0a332a72f6df58ee
gunheelee-c106-20 <qsd0812@me.com>
2024-06-10 19:08:19 +0900

    6/10

--- Project8/FileName.cpp
+++ Project8/FileName.cpp
@@ -1,37 +1,38 @@
 #include <array>
 #include <algorithm>
 #include <vector>
+#include <random>
+#include <numeric>
+#include <print>
 #include <list>
 #include <ranges>
 #include "String.h"
 #include "save.h"
 
 //-----------------------------------------------------------------
-// 2024.04.30 1학기 stl				                        (14주 2)
+// 2024.04.30 1학기 stl				                        (15주 1)
 //  
-// 반복자간의 거리를 재는 distance 함수를 잘 알아본다
-// c++20 concept / range
+// c++20 range
+// - [begin, end)
+// - begin + size
+// - [begin, predicate)
+// - [begin, ...)   무한
 //-----------------------------------------------------------------
 
 extern bool 관찰;
 
 
 
-template<class T>
-concept 숫자만 = is_integral_v<T> or is_floating_point_v<T>;
-
-template<숫자만 T>
-T add(T a, T b)
-{
-	return a + b;
-}
-
-
 int main()
 {
-	//문제 함수 템플릿 add를 만들어라
-	//save("FileName.cpp");
-	list<int> list{ 1,2,3 };
-	ranges::sort(list.begin(),list.end();
+	save("FileName.cpp");
+
+	vector<int>v;
+
+	// ADL(argument dependent lookup) 기능이 작동하지 않도록
+	// Eric Niebler
+	find(v.begin(), v.end(), 3);
+	ranges::find(v.begin(), v.end(), 3);
+
 
 }
