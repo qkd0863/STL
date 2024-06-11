8bd308400df8605d9e8c834646113151e23854cc
qkd0863 <qkd7183@naver.com>
2024-06-11 14:54:11 +0900

    6/11

--- Project8/p.cpp
+++ Project8/p.cpp
@@ -10,141 +10,56 @@
 #include "String.h"
 #include "save.h"
 
-//-----------------------------------------------------------------
-// 2024.04.16 1학기 stl				                        (7주 2)
-//  
-// deque - vecto와 list의 중간형태를 띄는 자료구조다
-//-----------------------------------------------------------------
 
 extern bool 관찰;
 
-class Test
-{
-	char x[4096];
-};
 
 
 default_random_engine dre;
 uniform_int_distribution uidNum{ 1,9999 };
 uniform_int_distribution<int> uidChar{ 'a','z' };
 
-class Dog
+
+
+//void Player::read(istream& is)
+//{
+//	is.read((char*)this, sizeof(Player));
+//	p.release();
+//	p = make_unique<char[]>(num);
+//	is.read((char*)p.get(), num);
+//}
+
+
+template <class b ,class val>
+b my_find(b begin, b end, val v)
 {
-	char c{ (char)uidChar(dre) };
-	int num{ uidNum(dre) };
-public:
-	int getN()const { return num; }
-
-	void show()const
+	while (begin != end)
 	{
-		cout << c << " - " << num << endl;
+		if (*begin == v)
+			return begin;
+		++begin;
 	}
-
-	friend ostream& operator<<(ostream& os, const Dog& dog)
-	{
-		return os << "글자:" << dog.c << ", 숫자" << dog.num;
-	}
-
-	friend bool operator<(const Dog& lhs, const Dog& rhs)
-	{
-		return lhs.num < rhs.num;
-	}
-
-
-	friend istream& operator>>(istream& is, Dog& dog)
-	{
-		return is.read((char*)&dog, sizeof(Dog));
-	}
-
-
-};
-
-
-
-void saveArrayToBinaryFile(const array<array<Dog, 100>, 100>& d, const string& filename)
-{
-	ofstream file(filename, ios::binary);
-
-	if (file.is_open())
-	{
-		for (const auto& row : d)
-		{
-			for (const auto& dog : row)
-			{
-				file.write(reinterpret_cast<const char*>(&dog), sizeof(Dog));
-			}
-		}
-
-		file.close();
-		cout << "Saved to " << filename << " successfully!" << endl;
-	}
-	else
-	{
-		cout << "Failed to open file " << filename << " for writing!" << endl;
-	}
+	return begin;
 }
-void loadArrayFromBinaryFile(array<array<Dog, 100>, 100>& d, const string& filename)
-{
-	ifstream file(filename, ios::binary);
-
-	if (file.is_open())
-	{
-		for (auto& row : d)
-		{
-			for (auto& dog : row)
-			{
-				file >> dog;
-			}
-		}
-
-		file.close();
-		cout << "Loaded from " << filename << " successfully!" << endl;
-	}
-	else
-	{
-		cout << "Failed to open file " << filename << " for reading!" << endl;
-	}
-}
-void sortArray(array<Dog, 100>& arr)
-{
-	sort(arr.begin(), arr.end());
-}
-void sortArrayOfArrays(array<array<Dog, 100>, 100>& d)
-{
-	for (auto& arr : d)
-	{
-		sortArray(arr);
-	}
-}
-
 
 
 int main()
 {
-	//문제 a에 "FileName.cpp"에 있는 단어를 저장하라
-	//a를 길이 오름차순으로 정렬하라
-	//화면에 a를 출력하라
-	
-	
+	String s{ "20240513 - stl container iterator algorithm" };
 
-	array<int, 26> alpha{};
+	// 문제 사용자가 찾는 글자가 s에 있는지 알려주자
 
-
-	char c;
-
-	ifstream in{ "Dog.txt",ios::binary };
-
-
-	array<Dog, 100>read;
-	
-	Dog temp;
-	int count = 1;
-	in.read((char*)read.data(), sizeof(Dog) * read.size());
+	while (1)
+	{
+		cout << "찾을 문자는?";
+		char ch;
+		cin >> ch;
+		auto p = my_find(s.begin(), s.end(), ch);
+		if (p != s.end())
+			cout << "위치 - " << "을 찾음" << endl;
+		else
+			cout << ch << "는 s에 없는 글자입니다" << endl;
+	}
 
 
-	sort(read.begin(), read.end(), [](const Dog& a, const Dog& b) {return a.getN() < b.getN(); });
-	cout << "N 의 합 - " << accumulate(read.begin(), read.end(), 0LL, [](long long sum, Dog& d) {return sum + d.getN(); }) << endl;
-
-	for (const Dog& d : read)
-		cout << d << endl;
 }
